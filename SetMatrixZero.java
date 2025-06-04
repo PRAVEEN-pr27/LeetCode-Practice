7d403f181a7262fdc8ac075909dfae0ccf04bf14
PRAVEEN-pr27 <prpraveen04@gmail.com>
2025-06-04 13:37:14 +0530

    Create SetMatrixZero.java

---
+++ SetMatrixZero.java
@@ -0,0 +1,35 @@
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+       int row=matrix.length;
+       int col=matrix[0].length;
+       
+       boolean rows[]=new boolean[row];
+       boolean cols[]=new boolean[col];
+     for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            if(matrix[i][j]==0){
+                rows[i]=true;
+                cols[j]=true;
+            }
+
+        }
+      }
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(rows[i] || cols[j])
+                 matrix[i][j]=0;
+            }
+        }
+       
+        for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            System.out.println(matrix[i][j]);
+        }
+        }
+            
+            
+
+        
+    }
+}
